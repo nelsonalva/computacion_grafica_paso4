d1e5e3908e458502d6981ef88e56c32b70f433b3
Nelson Álvarez <nelsonalva@gmail.com>
2022-05-18 22:05:02 -0500

    Creada transición para silla de sol

--- src/cg/paso4/transiciones3d/SillaSol.java
+++ src/cg/paso4/transiciones3d/SillaSol.java
@@ -16,30 +16,30 @@ import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
 import javax.media.j3d.Alpha;
 import javax.media.j3d.Background;
+import javax.media.j3d.BoundingBox;
 import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
 import javax.media.j3d.RotationInterpolator;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
-import javax.vecmath.AxisAngle4f;
 import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
 
-public class Asador extends Applet {
+public class SillaSol extends Applet {
     
     // Parámetros necesarios para crear el escenario para los objetos 3D
     private SimpleUniverse universo = null;
     private Canvas3D lienzo = null;
     
     // Constantes
-    private final String FONDO = "src/img/fondos/fondo_asador.jpg";
-    private final String RUTA_OBJETO = "src\\img\\objetos\\asador.obj";
+    private final String FONDO = "src/img/fondos/fondo_sunchair.jpg";
+    private final String RUTA_OBJETO = "src\\img\\objetos\\sunchair.obj";
     private static final int FRAME_WIDTH = 1200;
     private static final int FRAME_HEIGHT = 800;
     
     // Inicializador
-    public Asador(){
+    public SillaSol(){
         // Configuración de container
         setLayout(new BorderLayout());
         GraphicsConfiguration gc = SimpleUniverse.getPreferredConfiguration();
@@ -67,7 +67,7 @@ public class Asador extends Applet {
         TransformGroup grupoPrincipal = new TransformGroup();
         Transform3D transformacion3d = new Transform3D();
         transformacion3d.setTranslation(new Vector3d(0.0,0.0,-1)); //define posicion del objeto
-        transformacion3d.setScale(0.002); //define el tamaño del objeto
+        transformacion3d.setScale(0.001); //define el tamaño del objeto
         grupoPrincipal.setTransform(transformacion3d);
         TransformGroup grupoProducto = new TransformGroup();
         grupoProducto.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
@@ -84,14 +84,14 @@ public class Asador extends Applet {
 
         
     // Método para definir la transición del objeto
-    private void definirTransición(TransformGroup producto) {
-        Alpha alpha = new Alpha(-1,20000);
-        Transform3D eje = new Transform3D();
-        eje.setRotation(new AxisAngle4f(1.3f,8.0f,7.0f,8.0f));
-        RotationInterpolator girar = new RotationInterpolator(alpha, producto, eje,0.0f,(float) Math.PI*(6.0f));
-        BoundingSphere fronteras = new BoundingSphere(new Point3d(0.0,0.0,0.0),1000.0);
-        girar.setSchedulingBounds(fronteras);
-        producto.addChild(girar);
+    private void definirTransición(TransformGroup escenaTG) {
+        Alpha rotacionAlpha = new Alpha(-1,Alpha.INCREASING_ENABLE,0,0,4000,0,0,0,0,0);
+        
+        Transform3D yAxis = new Transform3D();
+        RotationInterpolator rotar = new RotationInterpolator(rotacionAlpha,escenaTG,yAxis,0.0f,(float) Math.PI*2.0f);
+        BoundingBox limites = new BoundingBox(new Point3d(-1000,-1000,-1000),new Point3d(100,100,100));
+        rotar.setSchedulingBounds(limites);
+        escenaTG.addChild(rotar);
     }
     
     // Método para cargar el objeto
@@ -117,7 +117,7 @@ public class Asador extends Applet {
     }
         
     public static void main (String[] args){
-        Asador ejecutar = new Asador();
+        SillaSol ejecutar = new SillaSol();
         Frame ventana = new MainFrame(ejecutar, FRAME_WIDTH, FRAME_HEIGHT);
     }
 }
